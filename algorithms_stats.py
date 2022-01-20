c1db32ae49fe24d51458368b4e8bf5028a1c07e0
clement <clement.tamines@gmail.com>
2022-01-20 13:25:06 +0100

    Added code to time calls in the algorithms and get the proportion of realizable payoffs

--- algorithms_stats.py
+++ algorithms_stats.py
@@ -1,3 +1,4 @@
+import time
 from collections import deque, defaultdict
 
 from verification_algorithms import smaller_than, generate_smaller_payoffs, counter_example_exists, \
@@ -24,7 +25,13 @@ def direct_antichain_algorithm_statistics(automaton, nbr_objectives, colors_map,
 
     visited = defaultdict(int)
 
-    counter = 0
+    counter_realizable = 0
+
+    counter_realizable_losing = 0
+
+    realizable_times = []
+
+    realizable_losing_times = []
 
     while queue:
 
@@ -32,18 +39,34 @@ def direct_antichain_algorithm_statistics(automaton, nbr_objectives, colors_map,
 
         if not any(smaller_than(p, p_prime) for p_prime in pareto_optimal_payoffs):
 
-            if realizable(p, automaton, colors_map):
+            start_time_process = time.process_time()
 
-                counter += 2
+            is_realizable = realizable(p, automaton, colors_map)
+
+            end_time_process = time.process_time()
+
+            counter_realizable += 1
+
+            realizable_times.append(end_time_process-start_time_process)
+
+            if is_realizable:
 
                 pareto_optimal_payoffs.append(p)
 
-                if realizable(p, automaton, colors_map, losing_for_0=True):
+                start_time_process = time.process_time()
 
-                    return False, pareto_optimal_payoffs, counter
+                is_realizable_losing = realizable(p, automaton, colors_map, losing_for_0=True)
+
+                end_time_process = time.process_time()
+
+                counter_realizable_losing += 1
+
+                realizable_losing_times.append(end_time_process - start_time_process)
+
+                if is_realizable_losing:
+
+                    return False, pareto_optimal_payoffs, [counter_realizable, realizable_times], [counter_realizable_losing, realizable_losing_times]
             else:
-
-                counter += 1
 
                 for p_star in generate_smaller_payoffs(p):
 
@@ -53,7 +76,7 @@ def direct_antichain_algorithm_statistics(automaton, nbr_objectives, colors_map,
                         queue.append(p_star)
                         visited[p_star] = 1
 
-    return True, pareto_optimal_payoffs, counter
+    return True,  pareto_optimal_payoffs, [counter_realizable, realizable_times], [counter_realizable_losing, realizable_losing_times]
 
 
 def counter_example_based_statistics(automaton, nbr_objectives, colors_map):
@@ -68,13 +91,27 @@ def counter_example_based_statistics(automaton, nbr_objectives, colors_map):
 
     pareto_optimal_payoffs = []
 
-    counter = 0
+    counter_exists = 0
+
+    counter_dominated = 0
+
+    exists_times = []
+
+    dominated_times = []
 
     while True:
 
-        if counter_example_exists(nbr_objectives, automaton, colors_map, pareto_optimal_payoffs):
+        start_time_process = time.process_time()
 
-            counter += 2
+        counter_example_ex = counter_example_exists(nbr_objectives, automaton, colors_map, pareto_optimal_payoffs)
+
+        end_time_process = time.process_time()
+
+        counter_exists += 1
+
+        exists_times.append(end_time_process - start_time_process)
+
+        if counter_example_ex:
 
             # the call to counter_example_exists modifies the acceptance condition of the automaton to check for a
             # counter example (run losing for Player 0 with payoff not dominated by a payoff in pareto_optimal_payoffs).
@@ -83,7 +120,17 @@ def counter_example_based_statistics(automaton, nbr_objectives, colors_map):
 
             counter_example_payoff = get_payoff_of_accepting_run(nbr_objectives, automaton, colors_map)
 
-            if counter_example_dominated(nbr_objectives, automaton, colors_map, counter_example_payoff):
+            start_time_process = time.process_time()
+
+            counter_example_dom = counter_example_dominated(nbr_objectives, automaton, colors_map, counter_example_payoff)
+
+            end_time_process = time.process_time()
+
+            counter_dominated += 1
+
+            dominated_times.append(end_time_process - start_time_process)
+
+            if counter_example_dom:
 
                 # the call to counter_example_dominated modifies the acceptance condition of the automaton to check
                 # whether the counter example payoff is dominated by some other payoff (winning for Player 0 and
@@ -99,14 +146,12 @@ def counter_example_based_statistics(automaton, nbr_objectives, colors_map):
 
                 # a counter example is not dominated by another payoff, hence the instance is false
 
-                return False, pareto_optimal_payoffs, counter
+                return False, pareto_optimal_payoffs, [counter_exists, exists_times], [counter_dominated, dominated_times]
         else:
-
-            counter += 1
 
             # there are no counter examples, hence the instance is true
 
-            return True, pareto_optimal_payoffs, counter
+            return True, pareto_optimal_payoffs, [counter_exists, exists_times], [counter_dominated, dominated_times]
 
 
 def compute_antichain(automaton, nbr_objectives, colors_map, realizable):
@@ -165,7 +210,9 @@ def compute_losing_payoffs(automaton, nbr_objectives, colors_map, realizable):
 
     maximal_payoff = tuple([1] * nbr_objectives)
 
-    pareto_optimal_payoffs = []
+    all_realizable_payoffs = []
+
+    losing_payoffs = []
 
     queue = deque()
 
@@ -179,7 +226,11 @@ def compute_losing_payoffs(automaton, nbr_objectives, colors_map, realizable):
 
         if realizable(p, automaton, colors_map, losing_for_0=True):
 
-            pareto_optimal_payoffs.append(p)
+            losing_payoffs.append(p)
+
+        if realizable(p, automaton, colors_map):
+
+            all_realizable_payoffs.append(p)
 
         for p_star in generate_smaller_payoffs(p):
 
@@ -188,4 +239,4 @@ def compute_losing_payoffs(automaton, nbr_objectives, colors_map, realizable):
                 queue.append(p_star)
                 visited[p_star] = 1
 
-    return pareto_optimal_payoffs
+    return all_realizable_payoffs, losing_payoffs
